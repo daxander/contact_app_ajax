0e0aea79b749c2fcac3df9e964f0013dbf366d6a
David Feimer <davidfeimer@gmail.com>
2015-05-06 03:42:11 +0000

    update actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,26 @@
 # Homepage (Root path)
 get '/' do
+  @contacts = Contact.all
   erb :index
+
 end
+
+get '/new' do
+  contact = Contact.new
+  content_type :json
+    contacts.to_json
+end
+
+get '/contacts' do
+  contacts = Contact.all
+  content_type :json
+    contacts.to_json
+end
+
+get '/new' do
+  @contact = Contact.new(name: params[:name], email: params[:email], phone_number: params[:phone_number])
+  if @contact.save
+    redirect to('/index')
+  end
+end
+
